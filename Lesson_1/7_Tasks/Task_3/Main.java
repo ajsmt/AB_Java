3ea7ba323e0572031cb9c73ce3c81d809637f2d5
ArpiJokle <ab@beltech.info>
2024-09-03 23:09:10 +0300

    Task 3 solution

--- Lesson_1/7_Tasks/Task_3/Main.java
+++ Lesson_1/7_Tasks/Task_3/Main.java
@@ -1,5 +1,31 @@
+// Напишите программу, вводящую три целочисленных значения и выводящую
+// самое большое из них, используя только условную операцию. Сделайте то же
+// самое с помощью метода Math.mах() .
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        int a;
+        System.out.print("Input first number : ");
+        a = input.nextInt();
+        int b;
+        System.out.print("Input second number : ");
+        b = input.nextInt();
+        int c;
+        System.out.print("Input third number : ");
+        c = input.nextInt();
+        input.close();
+        System.out.println("Solution with if statements : ");
+        if (a > b && a > c) {
+            System.out.println("Answer : " + a);
+        } else if (b > a && b > c) {
+            System.out.println("Answer : " + b);
+        } else {
+            System.out.println("Answer : " + c);
+        }
+        System.out.println("Solution with max() function : ");
+        System.out.println("Answer : " + Math.max(Math.max(a, b), c));
     }
 }
