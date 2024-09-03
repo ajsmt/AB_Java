d7db42d9083e2f637f097f420e6241829621c0e6
ArpiJokle <ab@beltech.info>
2024-09-03 23:24:46 +0300

    Task 6 solution

--- Lesson_1/7_Tasks/Task_6/Main.java
+++ Lesson_1/7_Tasks/Task_6/Main.java
@@ -1,5 +1,18 @@
+// Напишите программу, вычисляющую факториал п ! = 1 х 2 * . . . х n, 
+// используя класс Biglnteger. Вычислите факториал числа 1000.
+
+import java.math.BigInteger;
+
 public class Main {
     public static void main(String[] args) {
+        int n = 1000;
+        System.out.println(n + "! is : " + FactorialCalculator(n));
+    }
 
+    public static BigInteger FactorialCalculator(int n) {
+        BigInteger factorial = BigInteger.ONE;
+        for (int i = 2; i <= n; i++)
+            factorial = factorial.multiply(BigInteger.valueOf(i));
+        return factorial;
     }
 }
