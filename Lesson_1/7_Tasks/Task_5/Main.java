9fbf04f9012899cd14be7d0646700a5cf1fef07f
ArpiJokle <ab@beltech.info>
2024-09-03 23:19:45 +0300

    Task 5 solution

--- Lesson_1/7_Tasks/Task_5/Main.java
+++ Lesson_1/7_Tasks/Task_5/Main.java
@@ -1,5 +1,15 @@
+// Что произойдет, если привести числовое значение типа double к значению
+// типа int, которое больше самого большого значения типа int? Попробуйте
+// сделать это
+
 public class Main {
     public static void main(String[] args) {
-
+        double maxDouble = Double.MAX_VALUE;
+        int intNum = (int) maxDouble;
+        System.out.println("Max double value : " + maxDouble);
+        System.out.println("Max double value to int : " + intNum);
+        System.out.println(maxDouble > intNum);
+        // При приведении double к int результат максимальное значение int
+        // а все биты которые за пределами размера int игнорируются
     }
 }
