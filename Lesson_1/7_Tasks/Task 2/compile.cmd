b23445754f6565f0c9b52e57c5133a4f31ee6636
ArpiJokle <ab@beltech.info>
2024-09-03 22:52:29 +0300

    First task solution

---
+++ Lesson_1/7_Tasks/Task 2/compile.cmd
@@ -0,0 +1,4 @@
+@ECHO OFF
+javac Main.java
+java Main
+::java Main 0.9 0.0001
---
+++ Lesson_1/7_Tasks/Task_1/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input int value : ");
+        int number = input.nextInt();
+        input.close();
+        System.out.println("Binary: " + Integer.toBinaryString(number));
+        System.out.println("Octal: " + Integer.toOctalString(number));
+        System.out.println("Hex: " + Integer.toHexString(number));
+        System.out.println("Reverse hex value: " + Float.toHexString(1 / (float)number));
+    }
+}
