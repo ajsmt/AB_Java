7e775c56315cbbdca1585668c2b1dd2911559b5c
ArpiJokle <ab@beltech.info>
2024-09-03 23:29:31 +0300

    Task 7 solution

--- Lesson_1/7_Tasks/Task_7/Main.java
+++ Lesson_1/7_Tasks/Task_7/Main.java
@@ -1,5 +1,23 @@
+// Напишите программу, вводящую два числа в пределах от 0 до 65535, сохраня
+// ющую их в переменных типа short и вычисляющую их сумму, разность, про
+// изведение, частное и остаток без знака, не преобразуя эти величины в тип int.
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.print("Input first short number: ");
+        short a = input.nextShort();
+        System.out.print("Input second short number: ");
+        short b = input.nextShort();
+        input.close();
 
+        System.out.println("Sum : " + (a + b));
+        System.out.println("Sub : " + (a - b));
+        System.out.println("Mul : " + (a * b));
+        System.out.println("Chastnoe : " + (a / b));
+        System.out.println("Ostatok : " + (a % b));
+        
     }
 }
