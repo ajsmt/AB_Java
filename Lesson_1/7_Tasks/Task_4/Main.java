2b58ac5a75ebd66f7d9073c5cc8c30e2010a93b3
ArpiJokle <ab@beltech.info>
2024-09-03 23:34:00 +0300

    Task 4 solution + End of Lesson 1

--- Lesson_1/7_Tasks/Task_4/Main.java
+++ Lesson_1/7_Tasks/Task_4/Main.java
@@ -1,5 +1,10 @@
+// Напишите программу, выводящую наименьшее и наибольшее положитель
+// ные значения типа double. Подсказка: воспользуйтесь методом Math.nextUp ()
+// из прикладного программного интерфейса Java API.
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Double min positive value: " + Math.nextUp(.0));
+        System.out.println("Double max positive value: " + Double.MAX_VALUE);
     }
 }
